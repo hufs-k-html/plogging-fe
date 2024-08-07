19320eab0513169751ba561beb24f0e49119fd2b
yiseoffline <lsy0476@hufs.ac.kr>
2024-08-08 04:02:52 +0900

    fix color tailwind error

--- app/(tabs)/pages/Home.jsx
+++ app/(tabs)/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
@@ -14,20 +14,47 @@ const Home = () => {
         <Text className="pl-2 mb-3">현재까지</Text>
         <View className="items-center">
           <Text className="text-lg">
-            <Text className="text-green-500 font-bold">10.4 km</Text>를 걸으면서
+            <Text style={styles.green} className="font-bold text-xl">
+              10.4 km
+            </Text>
+            를 걸으면서
           </Text>
           <Text className="text-lg">
-            <Text className="text-green-500 font-bold">107개</Text>의 쓰레기를
-            주웠어요 !
+            <Text style={styles.green} className="font-bold text-xl">
+              107개
+            </Text>
+            의 쓰레기를 주웠어요 !
           </Text>
         </View>
       </View>
-      <TouchableOpacity className="bg-green-500 w-40 h-40 rounded-full mt-36 justify-center items-center">
-        <Text className="text-white text-2xl font-bold">플로깅</Text>
-        <Text className="text-white text-2xl font-bold">시작하기</Text>
+      <TouchableOpacity
+        style={styles.startPlogging}
+        className="w-40 h-40 rounded-full mt-36 justify-center items-center"
+      >
+        <Text
+          style={styles.startText}
+          className="text-white text-2xl font-bold"
+        >
+          플로깅
+        </Text>
+        <Text style={styles.startText} className="text-2xl font-bold">
+          시작하기
+        </Text>
       </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  green: {
+    color: '#2DB400',
+  },
+  startPlogging: {
+    backgroundColor: '#2DB400',
+  },
+  startText: {
+    color: '#fff',
+  },
+});
 
 export default Home;
