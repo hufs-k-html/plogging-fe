cf01122c5a7593ea1cff691e78f8264b1d6d711d
yiseoffline <lsy0476@hufs.ac.kr>
2024-08-08 02:20:06 +0900

    feat: create home page

--- app/(tabs)/pages/Home.jsx
+++ app/(tabs)/pages/Home.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
-const Start = () => {
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const plogging_image = require('../../../assets/image/homePlogging.png');
+
+const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 bg-white items-center">
+      <Image source={plogging_image} className="resize-contain" />
+      <View className="border border-black rounded-xl w-4/5 p-3 bg-white absolute top-64">
+        <Text className="pl-2 mb-3">현재까지</Text>
+        <View className="items-center">
+          <Text className="text-lg">
+            <Text className="text-green-500 font-bold">10.4 km</Text>를 걸으면서
+          </Text>
+          <Text className="text-lg">
+            <Text className="text-green-500 font-bold">107개</Text>의 쓰레기를
+            주웠어요 !
+          </Text>
+        </View>
+      </View>
+      <TouchableOpacity className="bg-green-500 w-40 h-40 rounded-full mt-36 justify-center items-center">
+        <Text className="text-white text-2xl font-bold">플로깅</Text>
+        <Text className="text-white text-2xl font-bold">시작하기</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-export default Start;
+export default Home;
