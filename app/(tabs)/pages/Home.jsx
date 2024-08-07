7e23147bc7271b0a803574eecc9454774f55e801
yiseoffline <lsy0476@hufs.ac.kr>
2024-08-08 04:18:04 +0900

    fix background color error

--- app/(tabs)/pages/Home.jsx
+++ app/(tabs)/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
@@ -7,10 +14,19 @@ const plogging_image = require('../../../assets/image/homePlogging.png');
 
 const Home = () => {
   return (
-    <View className="flex-1 bg-white items-center">
+    <SafeAreaView style={styles.safeAreaView} className="flex-1 items-center">
       <StatusBar style="dark" />
+      <View
+        style={styles.topContainer}
+        className="py-4 w-full border-b border-gray items-center"
+      >
+        <Text className="text-xl">플로깅 시작하기</Text>
+      </View>
       <Image source={plogging_image} className="resize-contain" />
-      <View className="border border-black rounded-xl w-4/5 p-3 bg-white absolute top-64">
+      <View
+        style={styles.current}
+        className="border rounded-xl w-4/5 p-3 bg-white absolute top-96"
+      >
         <Text className="pl-2 mb-3">현재까지</Text>
         <View className="items-center">
           <Text className="text-lg">
@@ -19,7 +35,7 @@ const Home = () => {
             </Text>
             를 걸으면서
           </Text>
-          <Text className="text-lg">
+          <Text className="text-lg mb-3">
             <Text style={styles.green} className="font-bold text-xl">
               107개
             </Text>
@@ -41,11 +57,17 @@ const Home = () => {
           시작하기
         </Text>
       </TouchableOpacity>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeAreaView: {
+    backgroundColor: '#fff',
+  },
+  current: {
+    backgroundColor: '#fff',
+  },
   green: {
     color: '#2DB400',
   },
@@ -54,6 +76,9 @@ const styles = StyleSheet.create({
   },
   startText: {
     color: '#fff',
+  },
+  topContainer: {
+    backgroundColor: '#fff',
   },
 });
 
